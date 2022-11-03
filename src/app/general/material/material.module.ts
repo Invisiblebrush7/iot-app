e912be461569a5a6117a770121e73a462837ec98
alberto romano <aromanog1@gmail.com>
2022-11-03 13:01:55 -0600

    Working on navbar

--- src/app/general/material/material.module.ts
+++ src/app/general/material/material.module.ts
@@ -4,10 +4,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule,
+  ],
 })
 export class MaterialModule {}
